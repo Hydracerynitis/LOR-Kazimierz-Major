37d42522bce362d6b0fd78d9e39f7fb149410ae3
czz071129 <czz071129@163.com>
2022-06-21 21:51:03 +0800

    加了xml

---
+++ SourceCode/NightMare/DiceCardAbility_targetPower_2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SN
+{
+	// Token: 0x0200000B RID: 11
+	public class DiceCardAbility_targetPower_2 : DiceCardAbilityBase
+	{
+		// Token: 0x06000045 RID: 69 RVA: 0x0000234F File Offset: 0x0000054F
+		public override void OnWinParrying()
+		{
+			base.behavior.TargetDice.card.ApplyDiceStatBonus(DiceMatch.AllDice, new DiceStatBonus() { power = -2 });
+		}
+	}
+}
